023418edc0610c8d5a15881cf3fb4a9314b15676
Birat <biratbasnet90@gmail.com>
2026-06-19 10:59:48 +0545

    Delete dashboard directory

--- dashboard/src/app/dashboard/[guildId]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from 'react';
-import DashboardShell from './DashboardShell';
-
-export default function GuildDashboardLayout({ children }: { children: ReactNode }) {
-  return <DashboardShell>{children}</DashboardShell>;
-}
